b6c47cbe68d48a84f38d99164084c31db577cba7
KimSori <noksm2@naver.com>
2024-09-24 02:37:34 +0900

    [feat] controller - 유저 포인트 충전, 사용, 조회 추가

--- src/main/java/io/hhplus/tdd/point/PointController.java
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -23,7 +23,7 @@ public class PointController {
     public UserPoint point(
             @PathVariable long id
     ) {
-        return new UserPoint(id, 0, 0);
+        return pointService.getPoints(id);
     }
 
     /**
@@ -33,7 +33,7 @@ public class PointController {
     public List<PointHistory> history(
             @PathVariable long id
     ) {
-        return List.of();
+        return pointService.chargeUsePoints(id);
     }
 
     /**
@@ -55,6 +55,6 @@ public class PointController {
             @PathVariable long id,
             @RequestBody long amount
     ) {
-        return new UserPoint(0, 0, 0);
+        return pointService.usePoints(id, amount);
     }
 }
