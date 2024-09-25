37e062fca71a2e4842609b3179b39593e4c73ef3
KimSori <noksm2@naver.com>
2024-09-25 22:16:27 +0900

    [test] 통합테스트 구현 - 유저 포인트 조회 만

--- src/test/java/io/hhplus/tdd/PointServiceIntegrationTest.java
+++ src/test/java/io/hhplus/tdd/PointServiceIntegrationTest.java
@@ -1,6 +1,8 @@
 package io.hhplus.tdd;
 
 import io.hhplus.tdd.point.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -11,6 +13,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -32,21 +35,55 @@ public class PointServiceIntegrationTest {
     private PointHistoryRepository pointHistoryRepository;
 
     @Test
-    public void 유저_포인트_충전_성공_통합테스트() {
-        // Given
+    public void 유저_포인트_조회_통합테스트(){
+        // given
         long userId = 1L;
-        long pointCharge = 400L;
 
-        // When
-        UserPoint result = pointService.chargePoints(userId, pointCharge);
+        // when
+        UserPoint result = pointService.getPoints(userId);
 
-        // Then
-        assertEquals(pointCharge, result.point());
-        List<PointHistory> histories = pointHistoryRepository.selectAllByUserId(userId);
-        assertEquals(1, histories.size());
-        assertEquals(TransactionType.CHARGE, histories.get(0).type());
-        assertEquals(pointCharge, histories.get(0).amount());
+        // then
+        assertEquals(0, result.point());
     }
+
+//    @Test
+//    public void 유저_포인트_사용_통합테스트(){
+//        // given
+//        long userId = 1L;
+//        long pointCharge = 600L;
+//        long pointUse = 400L;
+//        pointService.chargePoints(userId, pointCharge); // 포인트 충전
+//
+//        // when
+//        UserPoint result = pointService.usePoints(userId, pointUse);
+//
+//        // then
+//        assertEquals(pointCharge - pointUse, result.point());
+//        List<PointHistory> histories = pointHistoryRepository.selectAllByUserId(userId);
+//        assertEquals(2, histories.size());
+//        assertEquals(TransactionType.USE, histories.get(1).type());
+//        assertEquals(pointUse, histories.get(1).amount());
+//    }
+//
+//    @Test
+//    public void 유저_포인트_충전_성공_통합테스트() {
+//        // Given
+//        long userId = 1L;
+//        long pointCharge = 400L;
+//
+//        // When
+//        UserPoint result = pointService.chargePoints(userId, pointCharge);
+//
+//        // Then
+//        assertEquals(pointCharge, result.point());
+//        List<PointHistory> histories = pointHistoryRepository.selectAllByUserId(userId);
+//        assertEquals(1, histories.size());
+//        assertEquals(TransactionType.CHARGE, histories.get(0).type());
+//        assertEquals(pointCharge, histories.get(0).amount());
+//    }
+
+    // TODO: 동시성 제어 - 유저 포인트 충전 통합 테스트
+    // 단위 테스트에서 실패 케이스에 대해 테스트 했기 때문에 통합테스트에서는 db에 제대로 저장 되는지 확인!
 
 
 
